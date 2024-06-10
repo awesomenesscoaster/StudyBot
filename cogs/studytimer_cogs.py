a93e8f9d89815e4973360a0469f05b151eb9bdcd
Rhyan <rhyan.shah@gmail.com>
2024-06-09 18:25:49 -0700

    Remove Timer + View live timer

--- cogs/studytimer_cogs.py
+++ cogs/studytimer_cogs.py
@@ -20,7 +20,9 @@ class StudyTimerCog(commands.Cog):
             description= 'Start a countdown for an event or a timer to help study. \n \n'
             'Here are all the current commands available for the timer directory: \n'
             '**StartTimer** - Starts a current timer \n'
-            '**ViewTimers** - See a list of all the current timers in the server ',
+            '**ViewTimers** - See a list of all the current timers in the server \n'
+            '**ViewLiveTimer** - See a live countdown of a specific timer \n'
+            '**RemoveTimer** - Remove a timer',
             color=discord.Color.from_rgb(209, 178, 243),
         )
         await ctx.send(embed=embed)
@@ -117,7 +119,7 @@ class StudyTimerCog(commands.Cog):
         server_timers = self.timers.get(server_id, {})
         message = ''
         if not server_timers:
-            await ctx.send('There are no current timers in the server')
+            await ctx.send('There are no current timers in the server.')
             return
 
         for topic, timer_info in server_timers.items():
@@ -137,6 +139,123 @@ class StudyTimerCog(commands.Cog):
     @commands.command()
     async def ViewTimers(self,ctx):
         await self.viewAllTimers(ctx)
+    
+    async def liveTimerPrompt(self,ctx):
+        server_id = ctx.guild.id
+        server_timers = self.timers.get(server_id, {})
+        if not server_timers:
+            await ctx.send('There are no current timers for the server.')
+            return
+        
+        listoftopics = ''
+        for topic in server_timers:
+            listoftopics += f"**{topic}**  \n"
+        
+        embed = discord.Embed(
+            title= 'Live Timer',
+            description= 'Which timer would you like to view live for the server? \n \n' + listoftopics,
+            color= discord.Color.from_rgb(209, 178, 243)
+        )
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def ViewLiveTimer(self,ctx):
+        await self.liveTimerPrompt(ctx)
+        
+        def check(message):
+            return message.author == ctx.author and message.channel == ctx.channel
+        
+        def validTopic(user_input):
+            server_id = ctx.guild.id
+            server_timers = self.timers.get(server_id, {})
+            if user_input in server_timers:
+                return True
+            return False   
+            
+        try:
+            message = await self.bot.wait_for('message', check=check, timeout = 15)
+            user_input = message.content
+            if validTopic(user_input):
+                await self.liveTimer(ctx, user_input)
+                return None
+            await ctx.send(f'Invalid topic: **{user_input}**. Command Canceled.')
+        except asyncio.TimeoutError:
+            await ctx.send('You took to long to respond. Command canceled.')
+        
+    async def liveTimer(self, ctx, topic: str):
+        server_id = ctx.guild.id
+        server_timers = self.timers.get(server_id, {})
+        embed = discord.Embed(
+            title= f'{topic} - Live Timer',
+            description= '',
+            color = discord.Color.from_rgb(209, 178, 243),
+        )
+        message = await ctx.send(embed=embed)
+        
+        async def updateTimer():
+            while True:
+                timer_info = server_timers[topic]
+                elapsed = datetime.now() - timer_info['start_time']
+                remaining = timedelta(seconds= timer_info['duration']) - elapsed
+                remaining_seconds = int(remaining.total_seconds())
+                if remaining_seconds <= 0:
+                    await message.edit(embed = discord.Embed(
+                        title = f"Time is Up",
+                        description= f'The timer for {topic} has ended.',
+                        color = discord.Color.from_rgb(209, 178, 243),
+                    ))
+                    break
+                
+                minutes,seconds = divmod(remaining_seconds, 60)
+                embed.description=  f"Time remaining for **{topic}**: {minutes} minutes and {seconds} seconds."
+                await message.edit(embed = embed)
+                await asyncio.sleep(1)
+        self.bot.loop.create_task(updateTimer())
+    
+    async def RemoveTimerPrompt(self, ctx):
+        server_id = ctx.guild.id
+        server_timers = self.timers.get(server_id, {})
+        
+        if not server_timers:
+            await ctx.send('There are no current timers in the server.')
+            return
+        
+        listofTopics = ''
+        for i in server_timers:
+            listofTopics += f"**{i}** \n"
+            
+        embed = discord.Embed(
+            title= 'Remove Timer',
+            description= 'Which timer would you like to remove? \n \n' + listofTopics,
+            color = discord.Color.from_rgb(209, 178, 243),
+        )
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def RemoveTimer(self, ctx):
+        await self.RemoveTimerPrompt(ctx)
+       
+        def check(message):
+            return message.author == ctx.author and message.channel == ctx.channel
+       
+        def validTopic(user_input):
+            server_id = ctx.guild.id
+            server_timers = self.timers.get(server_id, {})
+            if user_input in server_timers:
+                return True
+            return False   
+            
+        try:
+            message = await self.bot.wait_for('message', check=check, timeout = 15)
+            user_input = message.content
+            if validTopic(user_input):
+               server_id = ctx.guild.id
+               self.timers[server_id].pop(user_input)
+               await ctx.send(f'Timer for **{user_input}** has been removed.')
+               return None
+            await ctx.send(f'Invalid topic: **{user_input}**. Command Canceled.')
+        except asyncio.TimeoutError:
+            await ctx.send('You took to long to respond. Command canceled.')
         
 async def setup(bot):
     await bot.add_cog(StudyTimerCog(bot))
